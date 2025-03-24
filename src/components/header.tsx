8584eedd11c933129294ae329ac79a632dbbe474
jnerydesigner <jander.webmaster@gmail.com>
2025-03-24 10:04:35 -0400

    feat: button home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router";
+
 export const Header = () => {
   return (
     <header className="flex flex-col justify-center items-center bg-jandernery shadow-md">
       <div className="h-[100px]">
-        <img
-          src="/logo-jander-nery-dev-redondo.svg"
-          alt="logo jander nery dev"
-          className="h-full"
-        />
+        <Link to="/">
+          <img
+            src="/logo-jander-nery-dev-redondo.svg"
+            alt="logo jander nery dev"
+            className="h-full"
+          />
+        </Link>
       </div>
       <h1 className="text-2xl font-bold text-gray-800">Se Liga Dev LTDA</h1>
     </header>
